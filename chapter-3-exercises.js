cb96a484b87956a64ada94b9cf993a9fb1781eb1
Tabitha Lay Blagdon <tjlay23@gmail.com>
2016-01-21 14:56:19 -0800

    Update chapter-3-exercises.js

--- chapter-3-exercises.js
+++ chapter-3-exercises.js
@@ -55,3 +55,46 @@ console.log(isEven(75));
 // → false
 console.log(isEven(-1));
 // → ??
+
+/* Exercise 3: Bean Counting
+You can get the Nth character, or letter, from a string by writing "string".charAt(N), similar to how you get its length 
+with "s".length. The returned value will be a string containing only one character (for example, "b"). The first character has 
+position zero, which causes the last one to be found at position string.length - 1. In other words, a two-character string has 
+length 2, and its characters have positions 0 and 1.
+
+Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters 
+are in the string.
+
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that 
+is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.*/
+
+function countBs(word) {
+  return countChar(word, "B");
+};
+
+function countBs2(word) {
+  var numBs = 0;
+  for (var i = 0; i < word.length; i++) {
+    if (word.charAt(i) === "B") {
+      numBs += 1;
+    }
+  }
+  return numBs;
+};
+
+function countChar(word, char) {
+  var nums = 0;
+  for (var i = 0; i < word.length; i++) {
+    if (word.charAt(i) === char) {
+      nums += 1;
+    }
+  }
+  return nums;
+};
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countBs2("BBC"));
+// -> 2
+console.log(countChar("kakkerlak", "k"));
+// → 4

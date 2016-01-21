97964d10ed627ee14705c289dd68dc9aa9d25852
Tabitha Lay Blagdon <tjlay23@gmail.com>
2016-01-20 18:36:04 -0800

    Update chapter-2-exercises.js

--- chapter-2-exercises.js
+++ chapter-2-exercises.js
@@ -54,3 +54,21 @@ Passing this string to console.log should show something like this:
 # # # #
 When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any 
 size, outputting a grid of the given width and height. */
+
+function chessboard() {
+  var chessboard = "";
+  var size = 8;
+  
+  for (var i = 0; i < size; i++) {
+    for (var j = 0; j < size; j++) {
+      if ((i + j) % 2 === 0) {
+        chessboard += " ";
+      }
+      else {
+        chessboard += "#"; 
+      }
+    }
+    chessboard += "\n";
+  }
+  console.log(chessboard);
+}

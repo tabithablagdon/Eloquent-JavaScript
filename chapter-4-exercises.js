4a73f68393b56818dbc587a3b079437d181eae45
Tabitha Lay Blagdon <tjlay23@gmail.com>
2016-01-23 21:30:50 -0800

    Update chapter-4-exercises.js

--- chapter-4-exercises.js
+++ chapter-4-exercises.js
@@ -1,16 +1,29 @@
-/* Exercise 1:
+/* Exercise 1: Range = Write a range function that takes two arguments, start and end, and returns an array containing all the numbers 
+from start up to (and including) end.
 
+Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the previous program and see 
+whether it does indeed return 55.
 
-*/
+As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used to build up 
+the array. If no step is given, the array elements go up by increments of one, corresponding to the old behavior. The function call 
+range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces 
+[5, 4, 3, 2].  */
 
 // Your code here.
 
 function range(start, end) {
-  rangeArray = [];
+  var rangeArray = [];
   if (arguments.length === 3) {
     var step = Number(arguments[2]);
-    for (var i = start; i <= end; i += step) {
-      rangeArray.push(i);
+    if (step < 0) {
+      for (var i = start; i >= end; i += step) {
+        rangeArray.push(i);
+      }
+    }
+    else {
+      for (var i = start; i <= end; i += step) {
+        rangeArray.push(i);
+      }
     }
   }
   else {
@@ -29,9 +42,11 @@ function sum(numbers) {
   return total;
 };
 
+
+
 console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(range(5, 2, -1));
-// → [5, 4, 3, 2]
+// → [5, 4, 3, 2] (5, 2, -1)
 console.log(sum(range(1, 10)));
 // → 55

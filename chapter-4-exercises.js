8e8bae05883ac2930db81d730cd8e78eeb3f6f75
Tabitha Lay Blagdon <tjlay23@gmail.com>
2016-01-24 09:29:22 -0800

    Update chapter-4-exercises.js

--- chapter-4-exercises.js
+++ chapter-4-exercises.js
@@ -50,3 +50,37 @@ console.log(range(5, 2, -1));
 // → [5, 4, 3, 2] (5, 2, -1)
 console.log(sum(range(1, 10)));
 // → 55
+
+
+/* Exercise 2: Arrays have a method reverse, which changes the array by inverting the order in which its elements appear. For this 
+exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces
+a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: 
+it modifies the array given as argument in order to reverse its elements. Neither may use the standard reverse method.*/
+
+// Your code here.
+
+function reverseArray(array) {
+  var newArray = [];
+  for (var i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+function reverseArrayInPlace(array) {
+  var length = array.length;
+  for (var i = length - 1; i >= 0; i--) {
+    array.push(array[i]);
+  }
+  for (var j = 0; j < length; j++) {
+    array.shift();
+  }
+  return array;
+ };
+
+console.log(reverseArray(["A", "B", "C", "D", "F"]));
+// → ["C", "B", "A"];
+var arrayValue = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]

f7f9f97b7a45103ed623838a9881e026de0b05c4
Tabitha Lay Blagdon <tjlay23@gmail.com>
2016-01-26 18:33:41 -0800

    Update chapter-4-exercises.js

--- chapter-4-exercises.js
+++ chapter-4-exercises.js
@@ -82,3 +82,58 @@ var arrayValue = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
+
+/* Exercise 3: Write a function arrayToList that builds up a data structure like the previous one when given [1, 2, 3] as argument, 
+and write a listToArray function that produces an array from a list. Also write the helper functions prepend, which takes an element 
+and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and 
+returns the element at the given position in the list, or undefined when there is no such element.
+
+If you haven’t already, also write a recursive version of nth. */
+
+
+function arrayToList(array) {
+  for (var i = array.length - 1; i >= 0; i--) {
+    var newList = {
+      value: array[i],
+      rest: newList
+    }
+    if (newList.rest === undefined) {
+      newList.rest = null;
+    }
+  }
+  return newList;
+};
+
+function listToArray(list) {
+  newArray = [];
+  for (var i = list; i; i = i.rest) {
+    newArray.push(i.value);
+  }
+  return newArray;
+};
+
+function prepend(num, list) {
+  var newList = {
+    value: num,
+    rest: list
+  }
+  return newList;
+};
+
+function nth(list, num) {
+  newArray = [];
+  for (var i = list; i; i = i.rest) {
+    newArray.push(i.value);
+  }
+  return newArray[num];
+};
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
+

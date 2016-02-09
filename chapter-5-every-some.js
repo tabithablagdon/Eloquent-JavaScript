f0aedd60fc43235aa372a0a987a26724353ef339
Tee Blagdon <tjlay23@gmail.com>
2016-02-08 17:06:32 -0800

    Update chapter-5-every-some.js

--- chapter-5-every-some.js
+++ chapter-5-every-some.js
@@ -17,9 +17,9 @@ function every(array, predicate) {
   return true;
 };
 
-function some(array, compare) {
+function some(array, predicate) {
   for (var i = 0; i < array.length; i++) {
-    if (compare(array[i])) {
+    if (predicate(array[i])) {
      return true;
   	}
     else {
@@ -27,3 +27,13 @@ function some(array, compare) {
     }
   }
  };
+ 
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
+ 

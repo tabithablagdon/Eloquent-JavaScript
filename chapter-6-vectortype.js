2181499944939612595d6ea9396496ec69346a8a
Tee Blagdon <tjlay23@gmail.com>
2016-04-02 13:59:10 -0700

    Update chapter-6-vectortype.js

--- chapter-6-vectortype.js
+++ chapter-6-vectortype.js
@@ -12,7 +12,26 @@ the distance of the point (x, y) from the origin (0, 0).
 
 */
 
-var Vector = function() {
-
-
+var Vector = function(x, y) {
+  this.x = x;
+  this.y = y;
 };
+
+Vector.prototype.plus = function(vector) {
+  return new Vector((this.x + vector.x), (this.y + vector.y));
+};
+
+Vector.prototype.minus = function(vector) {
+  return new Vector((this.x - vector.x), (this.y - vector.y));
+};
+
+Object.defineProperty(Vector.prototype, "length", {
+  get: function() { return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));}
+});
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5

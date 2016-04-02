665883299eedf21ff722e8b11e39a46732a58a26
Tee Blagdon <tjlay23@gmail.com>
2016-04-02 14:06:17 -0700

    Create chapter-6-another-cell.js

---
+++ chapter-6-another-cell.js
@@ -0,0 +1,8 @@
+/*
+Eloquent JavaScript - Chapter 6 Exercises: Another cell
+
+Implement a cell type named StretchCell(inner, width, height) that conforms to the table cell interface described earlier 
+in the chapter. It should wrap another cell (like UnderlinedCell does) and ensure that the resulting cell has at least the 
+given width and height, even if the inner cell would naturally be smaller.
+
+*/
